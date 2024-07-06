8b2e5d5a24103707b3d4a1cceb7617f7929808e5
Shantanu <dekateshantanu015@gmail.com>
2024-07-06 19:03:22 +0530

    Adjusted form styling and added next and back button in form title

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,6 @@
 const Personal = () => {
+  const summaryString = `As a Principal Software Engineer, I excel in designing and developing robust and scalable software solutions ...`;
+
   return (
     <>
       <div className="personal-input-container">
@@ -11,7 +13,7 @@ const Personal = () => {
             Job Title
             <span> optional</span>
           </span>
-          <input type="text" placeholder="John Doe" />
+          <input type="text" placeholder="Principal Software Engineer" />
         </label>
         <label>
           <span>Email</span>
@@ -22,23 +24,35 @@ const Personal = () => {
             Phone Number
             <span> Not recommended</span>
           </span>
-          <input type="text" placeholder="John Doe" />
+          <input type="text" placeholder="944 704 8000" />
         </label>
       </div>
       <label className="summary-label">
         <span>
           Summary <span> not recommended</span>
         </span>
-        <textarea></textarea>
+        <textarea placeholder={summaryString}></textarea>
       </label>
     </>
+  );
+};
+
+const FormTitle = () => {
+  return (
+    <div className="form-title-div">
+      <h3>Personal Details</h3>
+      <div className="next-btn-div">
+        <button className="prev-btn">Back</button>
+        <button className="next-btn">Next</button>
+      </div>
+    </div>
   );
 };
 
 const Form = () => {
   return (
     <div className="form-container">
-      <h3>Personal Details</h3>
+      <FormTitle />
       <form className="form">
         <Personal />
       </form>
