2ce2368e739efadc0c5e9cbdaf41d92165271129
Shantanu <dekateshantanu015@gmail.com>
2024-07-06 19:26:36 +0530

    Added Sidebar and its styling
    -created Sidebar
    -added its styling
    -added fa6 icons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import {
+  FaUser,
+  FaEllipsis,
+  FaCircle,
+  FaEye,
+  FaBorderTopLeft,
+} from "react-icons/fa6";
+import "./SideBar.css";
 
 function SideBar() {
   return (
-    <div>
-      <h3>Sidebar</h3>
+    <div className="sidebar">
+      <div className="layout-div">
+        <div className="layout-btn-div">
+          <button type="button" className="layout-btn">
+            <FaUser />
+          </button>
+        </div>
+        <button type="button" className="layout-opt-btn">
+          <FaEllipsis />
+        </button>
+      </div>
+
+      <div className="mode-btn-div">
+        <button type="button" className="mode-btn">
+          <FaBorderTopLeft></FaBorderTopLeft>
+        </button>
+      </div>
+      <div className="prev-btn-div">
+        <button type="button" className="prev-btn">
+          <FaEye></FaEye>
+        </button>
+      </div>
     </div>
   );
 }
